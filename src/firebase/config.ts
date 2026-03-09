7447daa60da6aa862da8543256d2f7c04d42cde3
abderahim becherif <abderahime.pay@gmail.com>
2026-03-09 13:51:07 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2150682367-2f3b6",
+  "appId": "1:479854643384:web:6c1c4056bc7a8552788c4d",
+  "apiKey": "AIzaSyBKMEQhZpPlR2S7RqvDsixelmq_QWK8tXM",
+  "authDomain": "studio-2150682367-2f3b6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "479854643384"
+};
